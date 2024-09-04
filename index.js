a623299ec2027eec04511232048cd384303a2489
javierduquebakata <duque.informatica@bakata.es>
2024-09-04 08:25:43 +0100

    Refactorizando nombres

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ class OpenblockResourceServer extends Emitter{
 "${envOpenBlockExternalResources}"`);
             this._builtinResourcesPath = envOpenBlockExternalResources;
         } else {
-            const thirdPartyResourcesPath = path.join(this._builtinResourcesPath, '../../OpenBlockExternalResources');
+            const thirdPartyResourcesPath = path.join(this._builtinResourcesPath, '../../DigitalCoDeBlockExternalResources');
             if (fs.existsSync(thirdPartyResourcesPath)) {
-                console.info('The OpenBlockExternalResources folder is detected in the parent directory');
+                console.info('The DigitalCoDeBlockExternalResources folder is detected in the parent directory');
                 this._builtinResourcesPath = thirdPartyResourcesPath;
             }
         }
