9b507091685b2f3e38b4b720d176ac20da1267d8
javierduquebakata <duque.informatica@bakata.es>
2024-09-12 11:32:51 +0100

    Corregido error en clave referencia para traducción de descripción MentorBit

--- external-resources/devices/MentorBit/translations.js
+++ external-resources/devices/MentorBit/translations.js
@@ -9,7 +9,7 @@ function getInterfaceTranslations () {
     return {
         "es": {
             "MentorBit.name": "MentorBit",
-            "MentorBit.description": "MentorBit desarrollado por Digital CodeSign. Introduce bloques Scratch blocks para controlar MentorBit."
+            "MentorBit.description": "MentorBit desarrollado por Digital CodeSign. Introduce bloques Scratch para controlar MentorBit."
         },
         "en": {
             "MentorBit.name": "MentorBit",
@@ -17,15 +17,15 @@ function getInterfaceTranslations () {
         },
         "ru": {
             "MentorBit.name": "MentorBit",
-            "ChuChubot.description": "MentorBit, разработанный Digital CodeSign. Включает блоки Scratch для управления MentorBit."
+            "MentorBit.description": "MentorBit, разработанный Digital CodeSign. Включает блоки Scratch для управления MentorBit."
         },
         "zh-cn": {
             "MentorBit.name": "MentorBit",
-            "ChuChubot.description": "MentorBit 由 Digital CodeSign 开发。它结合了 Scratch 模块来控制 MentorBit。"
+            "MentorBit.description": "MentorBit 由 Digital CodeSign 开发。它结合了 Scratch 模块来控制 MentorBit。"
         },
         "zh-tw": {
             "MentorBit.name": "MentorBit",
-            "ChuChubot.description": "MentorBit 由 Digital CodeSign 開發。它結合了 Scratch 模組來控制 MentorBit。"
+            "MentorBit.description": "MentorBit 由 Digital CodeSign 開發。它結合了 Scratch 模組來控制 MentorBit。"
         }
     }
     ;
